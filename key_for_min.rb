48b39668ce6c72e6fa6afb2208d83e88f93e59c4
thoughtful-storage-6584 <csimon1127@gmail.com>
2020-09-20 18:39:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,10 @@
 
 def key_for_min_value(name_hash)
   smallest = 0
-  
+  retval = :eee
   name_hash.each {|key, value|
     if value < smallest
-      smallest = key
+      retval = key
     end
   }
   smallest
