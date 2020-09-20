5a52c809d4af6956279f43033812ec76480f5a63
thoughtful-storage-6584 <csimon1127@gmail.com>
2020-09-20 18:54:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  smallest = 100
+  smallest = 1000000
   retval = nil
   if name_hash.empty?
     nil
