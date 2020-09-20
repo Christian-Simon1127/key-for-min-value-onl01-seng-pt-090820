0d3955ff5f5ed8b3f0b651027f00996b2cd12862
thoughtful-storage-6584 <csimon1127@gmail.com>
2020-09-20 18:43:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,10 +1,9 @@
-require relative "pry"
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
   smallest = 0
-  retval = :eee
+  retval = nil
   name_hash.each {|key, value|
     if value < smallest
       retval = key
